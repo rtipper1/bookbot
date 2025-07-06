e2864c3e36f919a97efccb4b6ad861b23f579a0a
rtipper1 <rtipper@purdue.edu>
2025-07-05 20:07:02 -0400

    added print report

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_char_freq, get_char_table
+import sys
 
 def get_book_text(filepath):
 
@@ -7,16 +8,29 @@ def get_book_text(filepath):
         
     return file_contents
 
+def print_report(filepath, num_words, char_table):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for char in char_table:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+
+    print("============= END ===============")
 
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    filepath = "books/frankenstein.txt"
+    text = get_book_text(filepath)
     num_words = get_num_words(text)
     freqs = get_char_freq(text)
     char_table = get_char_table(freqs)
 
     print(f"{num_words} words found in the document")
-    print(char_table)
+    print_report(filepath, num_words, char_table)
 
 
 main()
