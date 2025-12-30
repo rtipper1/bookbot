424816610ee3b1f9b9c66eb2f2fa9e390d41bfac
Rory Tippery <rory.tippery1@outlook.com>
2025-12-30 17:38:37 -0500

    updated frequencies to only contain alphanumeric chars

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from stats import count_words
-
+from stats import count_words, get_char_frequencies, get_char_dicts
 
 
 def main():
     filepath = "books/frankenstein.txt"
     text = get_book_text(filepath)
     count = count_words(text)
-    print(f"Found {count} total words")
+    frequencies = get_char_frequencies(text)
+    char_dicts = get_char_dicts(frequencies)
+    print(char_dicts)
+    print_report(filepath, count, frequencies)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         text = f.read()
 
     return text
+
+def print_report(filepath, word_count, char_frequencies):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,4 +1,31 @@
-
 def count_words(text):
     words = text.split()
     return len(words)
+
+def get_char_frequencies(text):
+    frequencies = {}
+
+    for char in text:
+        if char.isalpha():
+            if char.lower() not in frequencies:
+                frequencies[char.lower()] = 1
+
+            else:
+                frequencies[char.lower()] += 1
+
+    return frequencies
+
+def get_char_dicts(frequencies):
+    dicts = []
+    for key, value in frequencies.items():
+        char_dict = {}
+        char_dict["char"] = key
+        char_dict["num"] = value
+        dicts.append(char_dict)
+
+    def sort_on(dicts):
+        return dicts["num"]
+
+    dicts.sort(reverse=True, key=sort_on)
+
+    return dicts
