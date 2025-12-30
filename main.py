70c469160fdb0b539600d8392838d0b753c9aea6
Rory Tippery <rory.tippery1@outlook.com>
2025-12-30 17:17:24 -0500

    completed get and count words

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def main():
+    filepath = "books/frankenstein.txt"
+    text = get_book_text(filepath)
+    count = count_words(text)
+    print(f"Found {count} total words")
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        text = f.read()
+
+    return text
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+if __name__ == "__main__":
+    main()
